e469aeb432a1801794f35dce20d560a19db187ba
Aryan Sharma <Ani@aryans-macbook-air.local>
2016-08-23 17:59:04 +0530

    Initial Commit

---
+++ s2t-Nuance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  s2t-Nuance
+//
+//  Created by Aryan Sharma on 23/08/16.
+//  Copyright © 2016 hootyapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
